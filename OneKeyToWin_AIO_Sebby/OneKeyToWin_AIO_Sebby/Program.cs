59ab3120b28b34d2fe04663b3a79f5d52f4f8c26
MrWenzoxfs <Dorukcrli@gmail.com>
2016-01-17 11:32:45 +0200

    Update Program.cs

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Program.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Program.cs
@@ -2,8 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using LeagueSharp;
-using LeagueSharp.Common;
+using EloBuddy;
+using EloBuddy.SDK;
 using SharpDX;
 using SPrediction;
 
