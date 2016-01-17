be65f451bfd4b8a1d00c5b29f90eca5d5e76fcb3
MrWenzoxfs <Dorukcrli@gmail.com>
2016-01-17 11:25:38 +0200

    Update OktwCommon.cs

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/OktwCommon.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/OktwCommon.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using LeagueSharp;
-using LeagueSharp.Common;
+using EloBuddy;
+using EloBuddy.SDK;
 using SharpDX;
 namespace OneKeyToWin_AIO_Sebby
 {
