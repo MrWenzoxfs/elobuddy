adc4f68490d50ad3fd6925b7a04e717a007cc999
MrWenzoxfs <Dorukcrli@gmail.com>
2016-01-17 11:22:07 +0200

    Update Activator.cs

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/Activator.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/Activator.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LeagueSharp.Common;
+using EloBuddy.SDK;
 using SharpDX;
 using System.Drawing;
-using LeagueSharp;
+using EloBuddy;
 
 namespace OneKeyToWin_AIO_Sebby
 {
